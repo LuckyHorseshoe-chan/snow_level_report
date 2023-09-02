c8bec11d56d769de1714058713a3cff6ab66403c
Lucky Horseshoe <lucky@ya.ru>
2023-09-02 13:40:22 +0300

    add docker

---
+++ db/init.sql
@@ -0,0 +1,28 @@
+CREATE TABLE sites (
+    site_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    comment VARCHAR(255)
+);
+CREATE TYPE BATCH_STATUS AS ENUM ('accepted', 'rejected');
+CREATE TABLE batches (
+    batch_id SERIAL PRIMARY KEY,
+    site_id INTEGER NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    createdAt TIMESTAMP,
+    processedAt TIMESTAMP,
+    mapping JSON,
+    status BATCH_STATUS,
+    comment VARCHAR(255),
+    FOREIGN KEY(site_id)
+        REFERENCES sites(site_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+    );
+CREATE TABLE data_points (
+    data_point_id SERIAL PRIMARY KEY,
+    batch_id INTEGER NOT NULL,
+    data JSON,
+    FOREIGN KEY (batch_id)
+        REFERENCES batches (batch_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
