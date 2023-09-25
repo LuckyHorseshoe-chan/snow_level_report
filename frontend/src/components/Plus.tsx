b06003964b5df7b9cc8734ad02a98ce72792cded
Lucky Horseshoe <lucky@ya.ru>
2023-09-25 11:50:13 +0300

    pull app without source

--- frontend/src/components/Plus.tsx
+++
@@ -1,8 +0,0 @@
-function Plus({onClick} : {onClick: any}){
-    return(
-        <li id="plus-block" className="item" onClick={onClick}>
-            <div id="plus"></div>
-        </li>
-    )
-}
-export default Plus;
